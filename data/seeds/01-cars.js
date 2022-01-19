f58fd5333ed068adf0fabd94d9a38c6bdddee077
Bobby Gomez <bobizzle@Bobbys-MacBook-Pro.local>
2022-01-18 19:06:09 -0600

    MVP

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,42 @@
 // STRETCH
+const cars = [
+    {
+        vin: '111111',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 21500,
+        title: 'clean',
+        transmission: 'manual',
+
+    },
+
+    {
+        vin: '111111',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 11500,
+        title: 'salvage',
+
+    },
+
+    {
+        vin: '111111',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000,
+        
+
+    },
+]
+
+// exports.seed = function(knex) {
+//     return knex('cars')
+//     .truncate().then(() => {
+//     return knex('cars').insert(cars)
+//     })
+// }
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
